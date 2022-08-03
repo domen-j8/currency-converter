4b1637261681de08fcb762611e476824393ff33d
Domen <jakofcicdomen@gmail.com>
2022-08-03 20:04:33 +0200

    primeng saga-purpel theme added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ButtonModule} from "primeng/button";
+import {InputNumberModule} from 'primeng/inputnumber';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    InputNumberModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
